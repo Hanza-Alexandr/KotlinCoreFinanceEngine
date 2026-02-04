f268e9539d162528a7fa2f8d46ef037ba4452093
alexandr <ilgizakim20480@gmail.com>
2026-02-04 16:23:35 +0700

    Убрал пока лишние методы и запросы

--- src/main/kotlin/viewmodels/OperationViewModel.kt
+++ src/main/kotlin/viewmodels/OperationViewModel.kt
@@ -5,14 +5,9 @@ import org.example.repository.OperationRepository
 
 class OperationViewModel(private val operationRepository: OperationRepository){
 
-
     fun getListOperationsByStorages(listStorageId: List<Long>): List<Operation>{
-
-        val listTo = operationRepository.getOperationsDaoByToStorages(listStorageId)
-        //val listFrom = operationRepository.getOperationsDaoByFromStorages(listStorageId)
         val list = operationRepository.getOperationsDaoByToStorages(listStorageId)
         return list
-        //return (listTo+listFrom).toSet().toList()
     }
 
 }
