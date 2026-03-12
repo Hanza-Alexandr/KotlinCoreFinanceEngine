ffd70d3369c06ff2f63e3b82e1dc7245ef80d1c2
alexandr <ilgizakim20480@gmail.com>
2026-03-12 13:06:51 +0700

    Прописал методы репозитория в StorageRepositorySQLDelight.kt для работы со storage

--- src/main/kotlin/model/repository/sqldelightdb/StorageRepositorySQLDelight.kt
+++ src/main/kotlin/model/repository/sqldelightdb/StorageRepositorySQLDelight.kt
@@ -5,6 +5,7 @@ import com.example.StorageEntity
 import com.example.StorageQueries
 import org.example.model.domain.ExistColor
 import org.example.model.domain.Currency
+import org.example.model.domain.NewStorage
 import org.example.model.domain.Storage
 import org.example.model.domain.TypeStorage
 import org.example.model.repository.IStorageRepository
@@ -30,6 +31,57 @@ class StorageRepositorySQLDelight(private val queries: StorageQueries): IStorage
                 ).toDomain()
             )
         }
+    }
+
+    override fun getById(id: Long): Storage? {
+        val querRes = queries.getStorageById(id).executeAsOneOrNull() ?: return null
+        return StorageEntity(
+            id = querRes.id,
+            name = querRes.name,
+            user_id = querRes.user_id,
+            currency = querRes.currency,
+            type_storage = querRes.type_storage,
+            note = querRes.note,
+            color_id = querRes.colorId,
+            is_statistics = querRes.is_statistics,
+            is_archive = querRes.is_archive
+        ).toDomain(
+            ColorEntity(
+                querRes.colorId,
+                querRes.colorOwnerId,
+                querRes.colorHex
+            ).toDomain()
+        )
+    }
+
+    override fun save(storage: Storage): Storage? {
+        return queries.updateStorage(
+            name = storage.name,
+            currency = storage.currency.toString(),
+            type_storage = storage.currency.toString(),
+            note = storage.note,
+            color_id = storage.color.id,
+            is_statistics = if(storage.isStatistics) 1L else 0,
+            is_archive = if(storage.isArchive) 1L else 0,
+            id = storage.id
+        ).executeAsOneOrNull()?.toDomain(storage.color)
+    }
+
+    override fun save(storage: NewStorage): Storage? {
+        return queries.insertStorage(
+            name = storage.name,
+            user_id = storage.userId,
+            currency = storage.currency.toString(),
+            type_storage = storage.typeStorage.toString(),
+            note = storage.note,
+            color_id = storage.color.id,
+            is_statistics = if(storage.isStatistics) 1L else 0,
+            is_archive = if(storage.isArchive) 1L else 0,
+        ).executeAsOneOrNull()?.toDomain(storage.color)
+    }
+
+    override fun delete(storage: Storage): Storage? {
+        TODO("Not yet implemented")
     }
 
     fun StorageEntity.toDomain(color: ExistColor): Storage{
